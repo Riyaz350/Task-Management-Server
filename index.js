ebac36c11d7e58ce6c0a1fd69e135ec31cc9bddd
riyaz <riyaz3582@gmail.com>
2023-12-21 14:48:40 +0600

    added posting user data to database

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 
-
 app.use(express.json())
+app.use(cors({
+
+    origin:[ 'http://localhost:5173',
+            'http://localhost:5174',
+            'https://6568263afe8d95692e5ee16a--cute-vacherin-eb6d20.netlify.app'
+           ],
+    credentials:true
+
+}
+))
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -24,7 +33,23 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-    
+    const users = client.db("Task-Management").collection("users");
+
+    // USERS
+
+    app.post(`/users`, async(req, res)=>{
+        const user = req.body
+        console.log(user)
+        const query = {email : req.body.email} 
+        const find = await users.findOne(query)
+        if(find){
+          return res.send  ({message: 'user already exists', insertedId : null})
+        }
+        const result = await users.insertOne(user)
+        res.send(result)
+      })
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
